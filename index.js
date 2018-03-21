23079877f9eca09b6092aa68d31d7cb713520769
Chad Foley <chad.foley@raleighnc.gov>
2018-03-21 16:26:49 -0400

    should create dummy SR now

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express'),
     request = require("request-promise");
 var app = express();
 let results = "";
+let testResults = "";
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -18,52 +19,54 @@ app.post('/geoeventlogger', function (request, response) {
     response.send(request.body);    // echo the result back
 
     this.results = request.body;
-    this.results = JSON.stringify(this.results);
-    console.log('results = ', this.results);
+    console.log('results = ', this.results.street);
 
-    // let srPostBody = {
-    //     callerFirstName: "Waze",
-    //     callerLastName: "Waze",
-    //     callerWorkPhone: "9196708062",
-    //     location: "",
-    //     callerEmail: "charles.foley@raleighnc.gov ",
-    //     problemSid: "263574",
-    //     x: "",
-    //     y: "",
-    //     details: "This Service Request was created by Waze",
-    //     submitTo: 293626,
-    //     callerAddress: "726 davenbury way",
-    //     callerCity: "cary",
-    //     callerState: "NC",
-    //     callerZip: "27513"
-    // };
+    this.testResults = JSON.stringify(this.results);
+    console.log('test results = ', this.testResults.street);
 
-    // srPostBody.x = this.results.geometry.x;
-    // srPostBody.y = this.results.geometry.y;
-    // srPostBody.location = this.results.street;
+    let srPostBody = {
+        callerFirstName: "Waze",
+        callerLastName: "Waze",
+        callerWorkPhone: "9196708062",
+        location: "",
+        callerEmail: "charles.foley@raleighnc.gov ",
+        problemSid: "263574",
+        x: "",
+        y: "",
+        details: "This Service Request was created by Waze",
+        submitTo: 293626,
+        callerAddress: "726 davenbury way",
+        callerCity: "cary",
+        callerState: "NC",
+        callerZip: "27513"
+    };
 
-    // var options = {
-    //     method: "POST",
-    //     url:
-    //       "http://rhsoatstapp1.ci.raleigh.nc.us:8182/RaleighAPI/cityworks/createServiceRequest/",
-    //     headers: {
-    //       "Content-Type": "application/json"
-    //     },
-    //     body: srPostBody,
-    //     json: true
-    //   };
+    srPostBody.x = this.results.geometry.x;
+    srPostBody.y = this.results.geometry.y;
+    srPostBody.location = this.results.street;
+
+    var options = {
+        method: "POST",
+        url:
+          "http://rhsoatstapp1.ci.raleigh.nc.us:8182/RaleighAPI/cityworks/createServiceRequest/",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: srPostBody,
+        json: true
+      };
       
-    //   function createServiceRequest() {
-    //     request(options)
-    //       .then(response => {
-    //         // Handle the response
-    //         console.log("response is ", response);
-    //       })
-    //       .catch(err => {
-    //         // Deal with the error
-    //         console.log("error is ", err);
-    //       });
-    //   }
+      function createServiceRequest() {
+        request(options)
+          .then(response => {
+            // Handle the response
+            console.log("response is ", response);
+          })
+          .catch(err => {
+            // Deal with the error
+            console.log("error is ", err);
+          });
+      }
 
 });
 

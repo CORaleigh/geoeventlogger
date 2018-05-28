9c4f5c74ed1eb2d11041ae7a226dbb414f230692
Chad Foley <chad.foley@raleighnc.gov>
2018-05-28 12:25:46 -0400

    added winston logger test

--- index.js
+++ index.js
@@ -12,32 +12,18 @@ app.use(bodyParser.json());
 app.post('/geoeventlogger', function (req, res) {
 
     const logger = winston.createLogger({
-        levels: {
-            trace: 0,
-            input: 1,
-            verbose: 2,
-            prompt: 3,
-            debug: 4,
-            info: 5,
-            data: 6,
-            help: 7,
-            warn: 8,
-            error: 9
-        },
-        colors: {
-            trace: 'magenta',
-            input: 'grey',
-            verbose: 'cyan',
-            prompt: 'grey',
-            debug: 'blue',
-            info: 'green',
-            data: 'grey',
-            help: 'cyan',
-            warn: 'yellow',
-            error: 'red'
-        }
-    });
-
+        level: 'info',
+        format: winston.format.json(),
+        transports: [
+          //
+          // - Write to all logs with level `info` and below to `combined.log` 
+          // - Write all logs error (and below) to `error.log`.
+          //
+          new winston.transports.File({ filename: 'error.log', level: 'error' }),
+          new winston.transports.File({ filename: 'combined.log' })
+        ]
+      });
+      
     logger.add(winston.transports.Console, {
         level: 'info',
         prettyPrint: true,

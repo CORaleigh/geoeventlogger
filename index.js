1e5be74245e7b24d2ef0c4a399de336bd2827e5e
Chad Foley <chad.foley@raleighnc.gov>
2018-05-28 11:58:28 -0400

    added winston logger

--- index.js
+++ index.js
@@ -1,73 +1,33 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    request = require("request-promise");
-var app = express();
-let results = "";
-let testResults = "";
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require("request-promise");
+const winston = require('winston');
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json());
 
-// app.use(function (req, res) {
-//     res.setHeader('Content-Type', 'text/plain')
-//     res.write('you posted:\n')
-//     res.end(JSON.stringify(req.body, null, 2))
-//   });
+app.post('/geoeventlogger', function (req, res) {
+    winston.configure({
+        transports: [
+            new(winston.transports.File)({
+                filename: 'somefile.log'
+            })
+        ]
+    });
 
-app.post('/geoeventlogger', function (request, response) {
-    console.log(request.body); // your JSON
-    response.send(request.body);    // echo the result back
+    winston.log('info', 'Hello distributed log files!');
 
-    // this.results = request.body;
-    // console.log('results = ', this.results.street);
+    console.log('req.body = ', req.body); // your JSON
+    // console.log('req.query.data = ', req.query.data);
+    // console.log('req.params.data = ', req.params.data);
 
-    // this.testResults = JSON.stringify(this.results);
-    // console.log('test results = ', this.testResults);
+    var output = JSON.stringify(request.body);
+    winston.log('info', output);
 
-    // let srPostBody = {
-    //     callerFirstName: "Waze",
-    //     callerLastName: "Waze",
-    //     callerWorkPhone: "9196708062",
-    //     location: "",
-    //     callerEmail: "charles.foley@raleighnc.gov ",
-    //     problemSid: "263574",
-    //     x: "",
-    //     y: "",
-    //     details: "This Service Request was created by Waze",
-    //     submitTo: 293626,
-    //     callerAddress: "726 davenbury way",
-    //     callerCity: "cary",
-    //     callerState: "NC",
-    //     callerZip: "27513"
-    // };
-
-    // srPostBody.x = this.results.geometry.x;
-    // srPostBody.y = this.results.geometry.y;
-    // srPostBody.location = this.results.street;
-
-    // var options = {
-    //     method: "POST",
-    //     url:
-    //       "http://rhsoatstapp1.ci.raleigh.nc.us:8182/RaleighAPI/cityworks/createServiceRequest/",
-    //     headers: {
-    //       "Content-Type": "application/json"
-    //     },
-    //     body: srPostBody,
-    //     json: true
-    //   };
-      
-    //   function createServiceRequest() {
-    //     request(options)
-    //       .then(response => {
-    //         // Handle the response
-    //         console.log("response is ", response);
-    //       })
-    //       .catch(err => {
-    //         // Deal with the error
-    //         console.log("error is ", err);
-    //       });
-    //   }
-
+    res.send(request.body); // echo the result back
 });
-
+console.log('listening on 3001');
 app.listen(3001);
